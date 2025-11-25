09d4323bc5f06bfbb04e976abd80ebccf8a1b0a6
lycpan233 <lycpan@foxmail.com>
2025-11-25 17:14:58 +0800

    feat: 新增 Astro 博客结构、页面、组件及相关配置，并添加大量图片资源。

---
+++ eslint.config.ts
@@ -0,0 +1,20 @@
+import js from "@eslint/js"
+import eslintConfigPrettier from "eslint-config-prettier"
+import eslintPluginAstro from "eslint-plugin-astro"
+import { defineConfig } from "eslint/config"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+
+export default defineConfig([
+  {
+    ignores: [".astro/", "dist/"],
+  },
+  js.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: { globals: globals.browser },
+  },
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  eslintConfigPrettier,
+])
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Dreamer"
+export const SITE_DESCRIPTION = "lycpan233's personal blog."
+export const SITE_URL = "https://lycpan233.top"
+export const GITHUB_HOME_URL = "https://github.com/lycpan233"
